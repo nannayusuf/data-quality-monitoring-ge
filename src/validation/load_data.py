e40332d9c914295a9b70f5ab75e7475007e6d82b
nannasci <nannayusuf@gmail.com>
2025-05-26 17:33:43 -0300

    feat: load Olist dataset using KaggleHub

--- src/validation/load_data.py
+++ src/validation/load_data.py
@@ -1,7 +1,6 @@
 import kagglehub
 from kagglehub import KaggleDatasetAdapter
-from great_expectations.data_context import DataContext
-from great_expectations.core.batch import RuntimeBatchRequest
+
 
 dataset = "olistbr/brazilian-ecommerce"
 file = "olist_orders_dataset.csv"
@@ -12,23 +11,5 @@ df = kagglehub.load_dataset(
     file
 )
 
-context = DataContext()
+print(df.head())
 
-batch_request = RuntimeBatchRequest(
-    datasource_name="olist_data",
-    data_connector_name="default_runtime_data_connector_name",
-    data_asset_name="olist_orders",
-    runtime_parameters={"batch_data": df},
-    batch_identifiers={"default_identifier_name": "test"}
-)
-
-validator = context.get_validator(
-    batch_request=batch_request,
-    expectation_suite_name="olist_suite"
-)
-
-validator.expect_column_values_to_not_be_null("product_id")
-
-validator.save_expectation_suite()
-
-print("✅ Validation complete!")
