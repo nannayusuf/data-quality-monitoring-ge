2b64ffed85075bce1986c0101f985757d0b26dfc
nannasci <nannayusuf@gmail.com>
2025-05-26 17:39:32 -0300

    feat: add RuntimeBatchRequest to pass dataset to Great Expectations

--- src/validation/load_data.py
+++ src/validation/load_data.py
@@ -13,3 +13,15 @@ df = kagglehub.load_dataset(
 
 print(df.head())
 
+from great_expectations.core.batch import RuntimeBatchRequest
+from great_expectations.data_context import DataContext 
+
+context = DataContext()
+
+batch_request = RuntimeBatchRequest(
+        datasource_name="olist_data",
+    data_connector_name="default_runtime_data_connector_name",
+    data_asset_name="olist_orders",
+    runtime_parameters={"batch_data": df},
+    batch_identifiers={"default_identifier_name": "test"}
+)
