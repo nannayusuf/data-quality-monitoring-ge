63ac2e574a1b35afa9a2a4c367345524589445a9
nannasci <nannayusuf@gmail.com>
2025-05-26 17:50:25 -0300

    chore: commit all modified files

---
+++ init_ge.py
@@ -0,0 +1,5 @@
+from great_expectations.data_context import FileDataContext
+
+FileDataContext.create(base_directory="great_expectations")
+
+print("Great Expectations Data Context criado com sucesso!")
--- src/validation/setup_datasource.py
+++ src/validation/setup_datasource.py
@@ -1,4 +1,15 @@
 from great_expectations.data_context import DataContext
 
-context = DataContext("great_expectations")
 context = DataContext()
+
+context.add_datasource(
+    name="olist_data",
+    class_name="Datasource",
+    execution_engine={"class_name": "PandasExecutionEngine"},
+    data_connectors={
+        "default_runtime_data_connector_name": {
+            "class_name": "RuntimeDataConnector",
+            "batch_identifiers": ["default_identifier_name"]
+        }
+    }
+)
